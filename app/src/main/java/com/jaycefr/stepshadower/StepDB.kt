00fcc4197f37691b6cf0e9371701e872225d2481
JayceFR <jaycejefferson.vicious@gmail.com>
2025-06-29 16:07:16 +0100

    created database for storing every day steps

---
+++ app/src/main/java/com/jaycefr/stepshadower/StepDB.kt
@@ -0,0 +1,62 @@
+package com.jaycefr.stepshadower
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+/*
+Usage
+
+val db = Room.databaseBuilder(
+    applicationContext,
+    AppDatabase::class.java, "database-name"
+).build()
+
+val stepDAO = db.stepDao()
+now you can use stepDAO to access the database
+
+while inserting data into the database, use the following code
+val today = LocalData.now()
+val row   = Step(today.toEpochDay().toInt(), steps = 10000)
+stepDAO.insertAll(row)
+
+ */
+
+@Entity
+data class Step(
+    @PrimaryKey val date : Int,
+    @ColumnInfo(name = "step") val step : Int
+)
+
+@Dao
+interface StepDAO{
+    @Query("Select * FROM step")
+    fun getAll() : List<Step>
+
+    @Query("Select * FROM step WHERE date = :date")
+    fun getStepByDate(date : Int) : Step
+
+    @Query("UPDATE step SET step = :step WHERE date = :date")
+    fun updateStep(date : Int, step : Int)
+
+    @Query("INSERT INTO step (date, step) VALUES (:date, :step)")
+    fun insertStep(date : Int, step : Int)
+
+    @Insert
+    fun insertAll(vararg steps : Step)
+
+    @Delete
+    fun delete(step : Step)
+
+}
+
+@Database(entities = [Step::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun stepDao() : StepDAO
+}
