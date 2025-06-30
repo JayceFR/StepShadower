fe973a87ad79f4eea66e76111f2ce6650ef17d0a
JayceFR <jaycejefferson.vicious@gmail.com>
2025-06-30 23:11:19 +0100

    added notification for incorrect password

--- app/src/main/java/com/jaycefr/stepshadower/AdminReceiver.kt
+++ app/src/main/java/com/jaycefr/stepshadower/AdminReceiver.kt
@@ -1,10 +1,19 @@
 package com.jaycefr.stepshadower
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.UserHandle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import androidx.core.content.edit
 
 class AdminReceiver : DeviceAdminReceiver(){
     override fun onEnabled(context: Context, intent: Intent) {
@@ -17,12 +26,42 @@ class AdminReceiver : DeviceAdminReceiver(){
         super.onDisabled(context, intent)
     }
 
+    fun showNotification(context : Context, msg : String){
+        val channelId = "fail_channel"
+        val rm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel =
+                NotificationChannel(channelId, "Fail Channel", NotificationManager.IMPORTANCE_HIGH)
+            rm.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle("Security alert")
+            .setContentText(msg)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        rm.notify(42, notification)
+
+    }
+
     override fun onPasswordFailed(
         context: Context,
         intent: Intent,
         user: UserHandle
     ) {
-        Toast.makeText(context, "Wrong password", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "Wrong password", Toast.LENGTH_SHORT).show()
+        val prefs = context.getSharedPreferences("failed_attempts", Context.MODE_PRIVATE)
+        val failedCount = prefs.getInt("count", 0) + 1
+        prefs.edit { putInt("count", failedCount) }
+
+        if (failedCount >= 3){
+            Toast.makeText(context, "3 failed attempts detected", Toast.LENGTH_SHORT).show()
+            showNotification(context, "3 failed attempts detected")
+        }
+
         super.onPasswordFailed(context, intent, user)
     }
 
@@ -31,7 +70,9 @@ class AdminReceiver : DeviceAdminReceiver(){
         intent: Intent,
         user: UserHandle
     ) {
-        Toast.makeText(context, "Welcome boss", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(context, "Welcome boss", Toast.LENGTH_SHORT).show()
+        val prefs = context.getSharedPreferences("failed_attempts", Context.MODE_PRIVATE)
+        prefs.edit { putInt("count", 0) }
         super.onPasswordSucceeded(context, intent, user)
     }
 }
